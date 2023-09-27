06c969a2df848128ac2a86b1ef3f4067d786f864
estefanyac15 <142949231+estefanyac15@users.noreply.github.com>
2023-09-26 18:25:40 -0600

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,57 +1,68 @@
-var bow , arrow,  background, redB, pinkB, greenB ,blueB ,arrowGroup;
+var bow , arrow,  scene;
 var bowImage, arrowImage, green_balloonImage, red_balloonImage, pink_balloonImage ,blue_balloonImage, backgroundImage;
 
-var score =0;
-function preload(){  
-  backgroundImage = loadImage("background0.png");
+var BallonExplote;
 
+var redB, blueB, pinkB, greenB, arrowB;
+
+var score=0;
+
+function preload(){
+
+  backgroundImage = loadImage("background0.png");
   arrowImage = loadImage("arrow0.png");
   bowImage = loadImage("bow0.png");
   red_balloonImage = loadImage("red_balloon0.png");
   green_balloonImage = loadImage("green_balloon0.png");
   pink_balloonImage = loadImage("pink_balloon0.png");
   blue_balloonImage = loadImage("blue_balloon0.png");
+  BallonExplote = loadSound("pinchazo-globo-revienta-.mp3");
+
 }
+
+
 
 function setup() {
   createCanvas(400, 400);
 
-  //crear fondo
+  //crea el fondo
   scene = createSprite(0,0,400,400);
   scene.addImage(backgroundImage);
   scene.scale = 2.5
 
-  // crear arco para disparar flechas
+  //crea el arco para disparar las flechas
   bow = createSprite(380,220,20,50);
   bow.addImage(bowImage); 
   bow.scale = 1;
 
-  score = 0  
-  redB= new Group();
-  greenB= new Group();
-  blueB= new Group();
-  pinkB= new Group();
-  arrowGroup= new Group();  
+   score = 0    
+
+  redB = new Group();
+  blueB = new Group();
+  pinkB = new Group();
+  greenB = new Group();
+  arrowB = new Group();
 }
 
 function draw() {
  background(0);
-  // mover el suelo
-  scene.velocityX = -3 
+  //fondo en movimiento
+    scene.velocityX = -3 
 
-  if (scene.x < 0){
-    scene.x = scene.width/2;
-  }
+    if (scene.x < 0){
+      scene.x = scene.width/2;
+    }
 
-  //mover el arco
+  //arco en movimiento
   bow.y = World.mouseY
 
-  // soltar la flecha al presionar la barra espaciadora
-  if (keyDown("space")) {
-    createArrow();  
+   //suelta la flecha cuando se presione la tecla de barra espaciadora
+  if (keyWentDown("space")) {
+    createArrow();
+
   }
 
-  //crear enemigos continuamente
+  //crea enemigos de forma continua
   var select_balloon = Math.round(random(1,4));
 
   if (World.frameCount % 100 == 0) {
@@ -64,39 +75,48 @@ function draw() {
     } else {
       pinkBalloon();
     }
-  }
+  } 
 
-  if (arrowGroup.isTouching(redB)) {
+  if(arrowB.isTouching(redB)){
+      redB.destroyEach();
+      score=score+10;
+      BallonExplote.play();
+    }
 
-    redB.destroyEach();
-    //redB.destroy();
-    //redB.Each();
-    //ballon.destroyEach();
+  if(arrowB.isTouching(blueB)){
+      blueB.destroyEach();
+      score=score+3;
+      BallonExplote.play();
+    }
 
-    arrowGroup.destroyEach();
-    score=score+1;
-  }
+  if(arrowB.isTouching(greenB)){
+      greenB.destroyEach();
+      score=score+4;
+      BallonExplote.play();
+    }
 
-  if (arrowGroup.isTouching(greenB)) {
-    greenB.destroyEach();
-    arrowGroup.destroyEach();
-    score=score+3;
-  }
+  if(arrowB.isTouching(pinkB)){
+      pinkB.destroyEach();
+      score=score+8;
+      BallonExplote.play();
+    }
 
-  if (arrowGroup.isTouching(blueB)) {
-    blueB.destroyEach();
-    arrowGroup.destroyEach();
-    score=score+2;
-  }
-
-  if (arrowGroup.isTouching(pinkB)) {
-    pinkB.destroyEach();
-    arrowGroup.destroyEach();
-    score=score+1;
-  }
 
   drawSprites();
   text("Puntuación: "+ score, 300,50);
+}
+
+
+//crea las flechas para el arco
+ function createArrow() {
+  var arrow = createSprite(100, 100, 60, 10);
+  arrow.addImage(arrowImage);
+  arrow.x = 360;
+  arrow.y=bow.y;
+  arrow.velocityX = -4;
+  arrow.lifetime = 100;
+  arrow.scale = 0.3;
+  arrowB.add(arrow);
 }
 
 function redBalloon() {
@@ -133,22 +153,4 @@ function pinkBalloon() {
   pink.lifetime = 150;
   pink.scale = 1
   pinkB.add(pink);
-}
-
-
-// Crear flechas para el arco
- function createArrow() {
-  var arrow= createSprite(100, 100, 60, 10);
-  arrow.addImage(arrowImage);
-  arrow.x = 360;
-  arrow.y=bow.y;
-  arrow.velocityX = -4;
-  arrow.lifetime = 100;
-  arrow.scale = 0.3;
-
-  //  arrowGroup.addGroup(arrow);
-  //arrow.add(arrowGroup);
-  //arrowGroup.add();
-  arrowGroup.add(arrow);
-
 }
